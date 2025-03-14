e46ad1943e3aa7d15e331b6eb8b6d2133992b6d8
Zack Hecht <zhecht7@gmail.com>
2025-03-13 23:34:41 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+import controllers
+
+app = Flask(__name__, template_folder='views')
+
+app.secret_key = os.urandom(24)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=3000, debug=True)
