ab2a80c06f08e6aee5a291dc7a49729468939dd7
Zack Hecht <zhecht7@gmail.com>
2025-04-05 12:15:47 -0400

    t

--- shared.js
+++ shared.js
@@ -48,7 +48,7 @@ const evBookFormatter = function(cell, params, rendered) {
 	const data = cell.getRow().getData();
 	if (data.prop == "separator") return "";
 
-	if (params.book) {
+	if (params.book && !params.book.includes("vs-")) {
 		const book = params.book.split("-")[0];
 		let line = data.bookOdds[book] || "0";
 		if (line.includes("/")) {
